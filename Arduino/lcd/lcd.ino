193aca8e224081f4b3ef781b427aae5f233a2085
H1rono <hronok66@gmail.com>
2021-01-14 18:32:12 +0900

    add: LiquidCrystal test

---
+++ Arduino/lcd/lcd.ino
@@ -0,0 +1,21 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup()
+{
+    lcd.init();
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print("Hello, world!");
+    lcd.setCursor(0, 1);
+    lcd.print("LCD (^^)/");
+}
+
+void loop()
+{
+    int t = millis() / 1000;
+    lcd.setCursor(10, 1);
+    lcd.print(t);
+}
