1e1fb497da350a746bbca0d4effa60ecfa3b1157
H1rono <hronok66@gmail.com>
2021-01-15 18:30:39 +0900

    update: echo test

--- Arduino/echo/echo.ino
+++ Arduino/echo/echo.ino
@@ -1,6 +1,6 @@
-#include <robo2019.h>
+#include <echo.h>
 
-robo::Echo e(6, 7);
+robo::Echo e(2, 3);
 
 void setup() {
     e.setup();
@@ -11,26 +11,3 @@ void loop() {
     Serial.println(e.read());
     delay(1000);
 }
-
-// const int pin = LED_BUILTIN;
-
-// bool b = false;
-
-
-// void setup() {
-//     Serial.begin(9600);
-//     pinMode(pin, OUTPUT);
-// }
-
-
-// void loop() {
-//     if (b) {
-//         digitalWrite(pin, HIGH);
-//         Serial.println("on");
-//     } else {
-//         digitalWrite(pin, LOW);
-//         Serial.println("off");
-//     }
-//     delay(500);
-//     b = !b;
-// }
