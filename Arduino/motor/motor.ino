427cffe55009e6dc2629698e8935a98b3ab0a807
H1rono <hronok66@gmail.com>
2021-01-19 18:31:17 +0900

    modified: motor test

--- Arduino/motor/motor.ino
+++ Arduino/motor/motor.ino
@@ -1,93 +1,23 @@
 #include <robo2019.h>
-#include <ArxContainer.h>
 
-// robo::Echo e_left(), e_right();
+using robo::motor;
+using robo::bno_wrapper;
 
-class LineSensor
+constexpr int power = 100;
+
+void setup()
 {
-private:
-    uint8_t in_pin;
-public:
-    LineSensor(uint8_t input_pin): in_pin(input_pin) {}
-
-    void setup()
-    {
-        pinMode(in_pin, INPUT);
-    }
-
-    int read()
-    {
-        return analogRead(in_pin);
-    }
-};
-
-arx::vector<double> ds{0, PI, -PI / 2, PI / 2, -PI / 6, PI * 5 / 6};
-LineSensor line_sensors[] = {
-    LineSensor(1),
-    LineSensor(3),
-    LineSensor(5)
-};
-
-int black_border = 0;
-
-bool iswhite(int v)
-{
-    return v > 650;
+    motor.setup();
+    bno_wrapper.setup();
 }
 
-void setup() {
-    Serial.begin(9600);
-    robo::motor.setup();
-    robo::bno055::setup();
-    for (LineSensor sensor : line_sensors) {
-        sensor.setup();
-        black_border += sensor.read();
+void loop()
+{
+    double dir = bno_wrapper.get_direction();
+    if (abs(dir) > PI / 6) {
+        motor.set.rotate(dir > 0, abs(dir) * 40 / PI + 10);
+    } else {
+        motor.set.stop();
     }
-    black_border /= 3;
-}
-
-void loop() {
-    double facing_dir, mov_dir;
-    int line_value;
-
-    BNO055: {
-        facing_dir = robo::bno055::get_direction();
-    }
-
-    ROTATE: {
-        if (abs(facing_dir) > PI / 6) {
-            bool cond = facing_dir > 0;
-            robo::motor.set.rotate(cond, (int8_t)(abs(50 * facing_dir / PI)) + 10);
-        }
-        goto END;
-    }
-
-    LINE: {
-        int left  = line_sensors[0].read(),
-            right = line_sensors[1].read(),
-            back  = line_sensors[2].read();
-        if (iswhite(left)) {
-            mov_dir = -PI / 2;
-            goto MOVE;
-        } else if (iswhite(right)) {
-            mov_dir = PI / 2;
-            goto MOVE;
-        } else if (iswhite(back)) {
-            mov_dir = 0;
-            goto MOVE;
-        }
-    }
-
-    DEFAULT_DIR: {
-        //int i = millis() / 1000 % 6;
-        mov_dir = -PI / 2;
-    }
-
-    MOVE: {
-        robo::motor.set.direction_and_speed(mov_dir, 70);
-    }
-
-    END:
-    //Serial.println(robo::motor.get.info());
-    delay(10);
+    //motor.set.all_motors(power, power, power, power);
 }
