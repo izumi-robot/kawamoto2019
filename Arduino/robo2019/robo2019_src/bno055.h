b99521dc8a7f736d142bffa38a5c20b4ec6dbbec
H1rono-iphone <hronok@gmail.com>
2021-01-25 12:50:32 +0000

    update robo2019: added pointer-using functions, provided comment, inherit Adafruit_BNO055

--- Arduino/robo2019/robo2019_src/bno055.h
+++ Arduino/robo2019/robo2019_src/bno055.h
@@ -14,21 +14,68 @@ namespace robo {
 class BNO_wrapper
 {
 private:
+    //! シングルトンオブジェクト
     static BNO_wrapper _singleton;
-    bool _started;
+
+    //! bno055を検知したかどうか
+    bool _detected;
+    //! bno055のインスタンス
     Adafruit_BNO055 _bno;
 
 private:
+    // シングルトンにする上で欠かせない部分
+    /**
+     * @brief コンストラクタ
+     */
     BNO_wrapper() : _bno(55) {}
+    /**
+     * @brief コピーコンストラクタ
+     */
     BNO_wrapper(const BNO_wrapper &) : _bno(55) {}
+    /**
+     * @brief デストラクタ
+     */
     ~BNO_wrapper() {}
+    /**
+     * @brief コピー代入
+     */
     BNO_wrapper& operator=(const BNO_wrapper &) {}
 
 public:
-    static BNO_wrapper& instance(void);
-    void setup(void);
-    double get_direction(void);
-    Adafruit_BNO055& bno(void);
+    /**
+     * @fn BNO_wrapper& instance()
+     * @brief シングルトンのインスタンス取得用
+     * @return インスタンスの参照
+     * @note robo::bno_wrapperに同じ参照が格納されている
+     */
+    static BNO_wrapper& instance();
+
+    /**
+     * @fn void setup();
+     * @brief bno055のセットアップを行う
+     * @note 全体のsetup内で呼ばないと他の機能が使えない
+     */
+    void setup();
+    /**
+     * @fn double get_direction()
+     * @brief 現在向いている方向をラジアンで取得
+     * @return 現在向いている方向
+     * @note ラジアンの値は、0を最初の向きとして、そこから正回転が反時計回りとなっている
+     */
+    double get_direction();
+    /**
+     * @fn void get_direction(double *dst)
+     * @brief 現在向いている方向をラジアンで取得
+     * @param[out] dst 現在向いている方向
+     * @note ラジアンの値は、0を最初の向きとして、そこから正回転が反時計回りとなっている
+     */
+    void get_direction(double *);
+    /**
+     * @fn Adafruit_BNO055& bno()
+     * @brief 保持しているbno055のインスタンスを取得
+     * @return 保持しているbno055のインスタンス
+     */
+    Adafruit_BNO055& bno();
 };
 
 BNO_wrapper BNO_wrapper::_singleton;
@@ -46,14 +93,25 @@ void BNO_wrapper::setup()
 
 double BNO_wrapper::get_direction()
 {
-    if (!this->_started) { return 0.; }
-    double dir_degree = this->_bno.getVector(Adafruit_BNO055::VECTOR_EULER).x();
+    if (!_detected) { return 0.; }
+    double dir_degree = _bno.getVector(Adafruit_BNO055::VECTOR_EULER).x();
     double dir_radian = (
         (0 <= dir_degree && dir_degree <= 180)
         ? dir_degree
         : dir_degree - 360
     ) * -PI / 180;
     return dir_radian;
+}
+void BNO_wrapper::get_direction(double *dst)
+{
+    double &res = *dst;
+    if (!_detected) { res = 0.; }
+    double dir_degree = _bno.getVector(Adafruit_BNO055::VECTOR_EULER).x();
+    res = (
+        (0 <= dir_degree && dir_degree <= 180)
+        ? dir_degree
+        : dir_degree - 360
+    ) * -PI / 180;
 }
 
 Adafruit_BNO055& BNO_wrapper::bno()
@@ -62,6 +120,97 @@ Adafruit_BNO055& BNO_wrapper::bno()
 }
 
 BNO_wrapper &bno_wrapper = BNO_wrapper::instance();
+
+class _BNO055 : public Adafruit_BNO055
+{
+private:
+    //! シングルトンオブジェクト
+    static _BNO055 _singleton;
+    //! bnoを検知したかどうか
+    bool _detected;
+
+private:
+    // シングルトンにする上で欠かせない部分
+    /**
+     * @brief コンストラクタ
+     */
+    _BNO055() : Adafruit_BNO055(55) {}
+    /**
+     * @brief コピーコンストラクタ
+     */
+    _BNO055(const _BNO055 &) : Adafruit_BNO055(55) {}
+    /**
+     * @brief デストラクタ
+     */
+    ~_BNO055() {}
+    /**
+     * @brief コピー代入
+     */
+    _BNO055& operator=(const _BNO055 &) {}
+
+public:
+    /**
+     * @fn _BNO055& instance()
+     * @brief シングルトンのインスタンスを取得する
+     * @return インスタンスの参照
+     * @note robo::_bno055に同じ参照が格納されている
+     */
+    static _BNO055& instance();
+
+    /**
+     * @fn void setup();
+     * @brief bno055のセットアップを行う
+     * @note 全体のsetup内で呼ばないと他の機能が使えない
+     */
+    void setup();
+    /**
+     * @fn double get_direction()
+     * @brief 現在向いている方向をラジアンで取得
+     * @return dst 現在向いている方向
+     */
+    double get_direction();
+    /**
+     * @fn void get_direction(double *dst)
+     * @brief 現在向いている方向をラジアンで取得
+     * @param[out] dst 現在向いている方向
+     * @note ラジアンの値は、0を最初の向きとして、そこから正回転が反時計回りとなっている
+     */
+    void get_direction(double *);
+};
+
+_BNO055& _BNO055::instance()
+{
+    return _singleton;
+}
+
+void _BNO055::setup()
+{
+    _detected = Adafruit_BNO055::begin();
+    Adafruit_BNO055::setExtCrystalUse(true);
+}
+
+double _BNO055::get_direction()
+{
+    if (!_detected) { return 0.; }
+    double dir_degree = Adafruit_BNO055::getVector(Adafruit_BNO055::VECTOR_EULER).x();
+    double dir_radian = (
+        (0 <= dir_degree && dir_degree <= 180)
+        ? dir_degree
+        : dir_degree - 360
+    ) * -PI / 180;
+    return dir_radian;
+}
+void _BNO055::get_direction(double *dst)
+{
+    double &res = *dst;
+    if (!_detected) { res = 0.; }
+    double dir_degree = Adafruit_BNO055::getVector(Adafruit_BNO055::VECTOR_EULER).x();
+    res = (
+        (0 <= dir_degree && dir_degree <= 180)
+        ? dir_degree
+        : dir_degree - 360
+    ) * -PI / 180;
+}
 
 } // namespace robo
 
--- Arduino/robo2019/robo2019_src/motor.h
+++ Arduino/robo2019/robo2019_src/motor.h
@@ -1,6 +1,6 @@
 /**
  * @file motor.h
- * @brief Daisen MCB 操作用のクラス
+ * @brief Daisen MCB 操作用のクラス定義
  */
 
 
@@ -52,34 +52,37 @@ private: // 内部型
         inline int8_t one_motor(uint8_t) const;
         /**
          * @fn void one_motor(int8_t *dst, uint8_t pin)
-         * @param[in] pin ピン番号
-         * @param[out] dst 結果
+         * @brief pinのモーターのパワーを取得する
+         * @param[in] pin モーターのピン番号
+         * @param[out] dst モーターのパワー
          */
-        inline void one_potor(int8_t *, uint8_t);
+        inline void one_motor(int8_t *, uint8_t);
         
         /**
          * @fn String power_str(uint8_t pin) const
-         * @brief mcbに流す用の文字列を取得する
+         * @brief パワー設定用の文字列を取得する
          * @param[in] pin モーターのピン番号
          * @return String mcbに流す用の文字列
-         * @example power_str(1)
          */
         String power_str(uint8_t) const;
         /**
          * @fn void power_str(String *, uint8_t pin)
-         * @param[in] pin ピン番号
+         * @brief パワー設定用の文字列を取得する
+         * @param[in] pin モーターのピン番号
          * @param[out] dst mcbに流す用の文字列
          */
         void power_str(String *, uint8_t);
         /** 
          * @fn String info() const
-         * @brief mcbに流す用の文字列のセットを取得
+         * @brief パワー設定用の文字列のセットを取得
          * @return "[power_str(1), power_str(2), power_str(3)]"
+         * @note デバッグ用
          */
         String info() const;
         /**
-         * fn void info(String *dst)
-         * param[out] 結果
+         * @fn void info(String *dst)
+         * @brief パワー設定用の文字列のセットを取得
+         * @param[out] dst "[power_str(1), power_str(2), power_str(3)]"
          */
         void info(String *);
     };
@@ -92,7 +95,13 @@ private: // 内部型
     private:
         Motor *_motor;
     public:
+        /**
+         * @brief デフォルトのコンストラクタ
+         */
         Set() : _motor(NULL) {}
+        /**
+         * @brief 引数のポインタで初期化
+         */
         Set(Motor *motor) : _motor(motor) {}
 
         /**
@@ -101,24 +110,79 @@ private: // 内部型
          * @param[in] pin ピン番号
          * @param[in] power パワー
          */
-        bool one_motor(uint8_t, int8_t);
+        void one_motor(uint8_t, int8_t);
 
-        bool all_motors(int8_t, int8_t, int8_t, int8_t);
+        /**
+         * @fn bool all_motors(int8_t a, int8_t b, int8_t c, int8_t d)
+         * @brief 全てのモーターのパワーを一気に設定する
+         * @param[in] a ピン番号1に設定するモーターのパワー
+         * @param[in] b ピン番号2に設定するパワー
+         * @param[in] c 番号3のパワー
+         * @param[in] d 4番のパワー
+         */
+        inline void all_motors(int8_t, int8_t, int8_t, int8_t);
+
+        /**
+         * @fn bool velocity(const double &vx, const double &vy)
+         * @brief ベクトル(vx, vy)が機体の速度ベクトルになるようにモーターのパワーを設定する
+         * @param[in] vx ベクトルのx成分
+         * @param[in] vy ベクトルのy成分
+         * @detail x成分は機体の正面方向が正方向、y成分は機体の左方向が正方向。いつもの座標系を反時計回りに90度回転させた感じ。こうすることで機体の正面が0度になり、方向の管理が楽になる。
+         */
         bool velocity(const double &, const double &);
-        bool velocity(const robo::V2_double &);
-        bool left_right(int8_t, int8_t);
+
+        /**
+         * @fn bool velocity(const robo::V2_double &v)
+         * @brief velocity(v.x, v.y)のショートカット
+         * @param[in] v 設定する速度ベクトル
+         * @sa bool velocity(const double vx, const double vy)
+         * @detail V2_doubleについてはvec2d.hを参照。
+         */
+        inline bool velocity(const robo::V2_double &);
+
+        /**
+         * @fn bool left_right(int8_t left, int8_t right)
+         * @brief 左輪(ピン番号1, 3)、右輪(ピン番号2, 4)でパワーを設定する
+         * @param[in] left  左輪のパワー
+         * @param[in] right 右輪のパワー
+         */
+        inline bool left_right(int8_t, int8_t);
+
+        /**
+         * @fn bool direction_and_speed(const double &direction, int8_t speed)
+         * @brief 与えられた方向と速さに機体の速度を設定する
+         * @param[in] direction 方向(ラジアン)
+         * @param[in] speed 速さ
+         * @detail 方向は機体の正面を0とし、反時計回りが正回転。PIまたは-PIで真後ろを指す。
+         */
         bool direction_and_speed(const double &, int8_t);
+
+        /**
+         * @fn bool rotate(bool clockwise, int8_t speed)
+         * @brief 回転するようにパワーを設定する。
+         * @param[in] clockwise 時計回りに回転するかどうか
+         * @param[in] speed 回転の速さ
+         */
         bool rotate(bool, int8_t);
+
+        /**
+         * @fn bool stop()
+         * @brief 停止させる
+         */
         bool stop();
         // TODO: 旋回運動
         // void circular(const double &rotate_vel, const int &vel=100);
     };
 
 private: // variables
+    //! シングルトンのオブジェクト
     static Motor _singleton;
+    //! モーターのパワー
     int8_t _powers[4];
 public:
+    //! 情報取得用の関数群
     Get get;
+    //! 更新用の関数群
     Set set;
 
 private: // functions
@@ -141,24 +205,61 @@ private: // functions
     Motor& operator=(const Motor &) { return *this; }
 
 public:
+    /**
+     * @fn Motor& instance()
+     * @brief シングルトンのインスタンス取得用
+     * @return インスタンスの参照
+     * @note robo::motorに同じ参照が格納されている
+     */
     static Motor& instance();
-    static String power_str(int, int8_t);
 
-    void stop();
+    /**
+     * @fn String power_str(uint8_t pin, int8_t power)
+     * @brief パワー設定用の文字列を取得する
+     * @param[in] pin モーターのピン番号
+     * @param[in] power モーターのパワー
+     * @return パワー設定用の文字列
+     * @detail 例えば、`power_str(1, -10)`は"1R010"を返す。実際のパワーは変更されない点に注意。
+     */
+    static String power_str(uint8_t, int8_t);
+    /**
+     * @fn void power_str(String *dst, uint8_t pin, int8_t power)
+     * @brief パワー設定用の文字列を取得する
+     * @param[in] pin モーターのピン番号
+     * @param[in] power モーターのパワー
+     * @param[out] dst パワー設定用の文字列
+     */
+    static void power_str(String *, uint8_t, int8_t);
+
+    /**
+     * @fn void setup();
+     * @brief モーターのセットアップを行う
+     * @note 全体のsetup内で呼ばないと他の機能が使えない。
+     */
     void setup();
 }; // class Motor
 
 Motor Motor::_singleton;
 
-inline int8_t Motor::Get::one_motor(uint8_t pin) const
+int8_t Motor::Get::one_motor(uint8_t pin) const
 {
-    return _motor->_powers[(pin - 1) & 3];
+    return _motor->_powers[(pin - 1)];
+}
+void Motor::Get::one_motor(int8_t *dst, uint8_t pin)
+{
+    if (dst != NULL) *dst = _motor->_powers[(pin - 1)];
 }
 
 String Motor::Get::power_str(uint8_t pin) const
 {
-    int8_t power = this->one_motor(pin);
+    int8_t power = one_motor(pin);
     return Motor::power_str(pin, power);
+}
+void Motor::Get::power_str(String *dst, uint8_t pin)
+{
+    if (dst == NULL) return;
+    int8_t power = one_motor(pin);
+    Motor::power_str(dst, pin, power);
 }
 
 String Motor::Get::info() const
@@ -170,53 +271,78 @@ String Motor::Get::info() const
     }
     return info_str + "]";
 }
-
-bool Motor::Set::one_motor(uint8_t pin, int8_t power)
+void Motor::Get::info(String *dst)
 {
-    int8_t &p = _motor->_powers[pin-1];
-    if (power < -100 || abs(p - power) < 5 || 100 < power) { return false; }
-    String dest = Motor::power_str(pin, power);
-    Serial2.println(dest);
+    if (dst == NULL) return;
+    String info_str = *dst;
+    info_str = "[";
+    for (uint8_t i = 1; i <= 4; i++) {
+        info_str += String(i) + ": " + this->power_str(i);
+        if (i != 4) { info_str += ", "; }
+    }
+    info_str += "]";
+}
+
+void Motor::Set::one_motor(uint8_t pin, int8_t power)
+{
+    int8_t &p = _motor->_powers[pin - 1];
+    if (p == power) return;
+    String p_str;
+    Motor::power_str(&p_str, pin, power);
+    Serial2.println(p_str);
     p = power;
-    return true;
 }
 
 bool Motor::Set::all_motors(int8_t a, int8_t b, int8_t c, int8_t d)
 {
-    return one_motor(1, a) & one_motor(2, b) & one_motor(3, c) & one_motor(4, d);
+    String powers_str;
+    int8_t ps[] = {a, b, c, d};
+    for (int i = 0; i < 4; ++i)
+    {
+        int8_t &power = _motor->_powers[i];
+        const int8_t &p = ps[i];
+        if (power == p) continue;
+        power = p;
+        String p_str;
+        Motor::power_str(&p_str, i + 1, p);
+        powers_str.concat(p_str);
+        powers_str.concat('\n');
+    }
+    Serial2.print(powers_str);
 }
 
-bool Motor::Set::velocity(const double &vx, const double &vy)
+void Motor::Set::velocity(const double &vx, const double &vy)
 {
     double root2 = sqrt(2.);
-    int8_t left = (int8_t)((vx + vy) / root2), right = (int8_t)((vx - vy) / root2);
-    return all_motors(right, left, left, right);
+    int8_t e1 = int8_t((vx + vy) / root2);
+    int8_t e2 = int8_t((vx - vy) / root2);
+    all_motors(e2, e1, e1, e2);
 }
 
-bool Motor::Set::velocity(const robo::V2_double &vel)
+void Motor::Set::velocity(const robo::V2_double &v)
 {
-    return velocity(vel.x, vel.y);
+    velocity(v.x, v.y);
 }
 
-bool Motor::Set::left_right(int8_t left, int8_t right)
+void Motor::Set::left_right(int8_t left, int8_t right)
 {
-    return all_motors(left, right, left, right);
+    all_motors(left, right, left, right);
 }
 
-bool Motor::Set::direction_and_speed(const double &direction, int8_t speed)
+void Motor::Set::direction_and_speed(const double &direction, int8_t speed)
 {
-    return velocity(speed * cos(direction), speed * sin(direction));
+    velocity(speed * cos(direction), speed * sin(direction));
 }
 
-bool Motor::Set::rotate(bool clockwise, int8_t speed)
+void Motor::Set::rotate(bool clockwise, int8_t speed)
 {
     int8_t d = clockwise ? 1 : -1;
-    return left_right(speed * d, -speed * d);
+    left_right(speed * d, -speed * d);
 }
 
-bool Motor::Set::stop()
+void Motor::Set::stop()
 {
-    return all_motors(0, 0, 0, 0);
+    all_motors(0, 0, 0, 0);
 }
 
 Motor& Motor::instance()
@@ -224,24 +350,27 @@ Motor& Motor::instance()
     return Motor::_singleton;
 }
 
-String Motor::power_str(int pin, int8_t power)
+String Motor::power_str(uint8_t pin, int8_t power)
 {
-    // R: front?
-    String dir_s = power < 0 ? "F" : "R";
-    String power_s = String(abs(power));
-    power_s = robo::string::rjust(power_s, 3, '0');
-    return String(pin) + dir_s + power_s;
+    char buff[8] = "";
+    char dir_c = power < 0 ? 'F' : 'R';
+    sprintf(buff, "%01d%c%03d", pin, dir_c, abs(power));
+    // R: 正転?
+    // String dir_s = power < 0 ? "F" : "R";
+    // String power_s = String(abs(power));
+    // power_s = robo::string::rjust(power_s, 3, '0');
+    return String(buff);
 }
-
-// void Motor::stop()
-// {
-//     this->set.all_motors(0, 0, 0, 0);
-// }
+void Motor::power_str(String *dst, uint8_t pin, int8_t power)
+{
+    char dir_c = power < 0 ? 'F' : 'R';
+    sprintf(dst->c_str(), "%01d%c%03d", pin, dir_c, abs(power));
+}
 
 void Motor::setup()
 {
     Serial2.begin(19200);
-    this->set.stop();
+    set.stop();
 }
 
 Motor &motor = Motor::instance();
