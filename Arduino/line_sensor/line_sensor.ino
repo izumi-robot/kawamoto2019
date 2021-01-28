4a8a628002a6348522e957cd41c38f4a9084b845
H1rono-iphone <hronok@gmail.com>
2021-01-28 11:46:33 +0000

    modified line sensor test

--- Arduino/line_sensor/line_sensor.ino
+++ Arduino/line_sensor/line_sensor.ino
@@ -4,6 +4,8 @@ using robo::motor;
 using robo::LineSensor;
 LineSensor left(1), right(3), back(5);
 
+bool changed = false;
+
 void setup()
 {
     Serial.begin(9600);
@@ -17,13 +19,14 @@ void setup()
 
 void loop()
 {
-    double dir = PI / 2;
+    static double dir = PI / 2;
     const bool
         lw = LineSensor::iswhite(left.read()),
         rw = LineSensor::iswhite(right.read()),
         bw = LineSensor::iswhite(back.read());
-    if (rw) {
+    if (lw && !changed) {
         dir *= -1;
+        changed = true;
     }
     motor.set.direction_and_speed(dir, 100);
 }
