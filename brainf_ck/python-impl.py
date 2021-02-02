35d7cd4080562869ce2e2c53d0f8ac70beb64b28
H1rono <hronok@gmail.com>
2021-02-02 11:58:34 +0000

    wrote brainf*ck in python

--- brainf_ck/python-impl.py
+++ brainf_ck/python-impl.py
@@ -1,5 +1,6 @@
 import enum
 
+
 class Command(enum.Enum):
     add = '+'
     sub = '-'
@@ -15,12 +16,36 @@ class Command(enum.Enum):
 
 
 class Machine(list):
-    __slots__ = ("_ptr",)
+    __slots__ = ("_ptr", "_mem_len")
+
+    def __init__(self, ptr=0, mem_len=1024):
+        self._ptr = ptr
+        self._mem_len = mem_len
 
     @property
     def val(self):
         return self[self._ptr]
 
-    @val.setter
-    def val(self, nv):
-        
+    @property
+    def ptr(self):
+        return self._ptr
+
+    @property
+    def mem_len(self):
+        return self._mem_len
+
+    def inc_val(self):
+        self[self._ptr] += 1
+        return self[self._ptr]
+
+    def dec_val(self):
+        self[self._ptr] -= 1
+        return self[self._ptr]
+
+    def inc_ptt(self):
+        self._ptr += 1
+        return self._ptr
+
+    def dec_ptr(self):
+        self._ptr -= 1
+        return self._ptr
