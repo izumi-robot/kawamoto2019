744f0b7188827f275f3c2afb7fb18d9559ebd398
H1rono <hronok66@gmail.com>
2021-02-10 18:31:52 +0900

    modified openmv test a little

--- openmv/openmv.ino
+++ openmv/openmv.ino
@@ -1,44 +1,7 @@
 #include <robo2019.h>
 
-using robo::I2CReaderWithAddr;
-using CameraPos = robo::Vector2D<uint16_t>;
-
-
-class OpenMVReader : public I2CReaderWithAddr
-{
-public:
-    using I2CReaderWithAddr::I2CReaderWithAddr;
-
-    /**
-     * @fn CameraPos read_pos(const CameraPos &)
-     */
-    CameraPos read_pos(const CameraPos &);
-};
-
-CameraPos OpenMVReader::read_pos(const CameraPos &pos_on_fail = CameraPos{0, 0})
-{
-    static constexpr uint16_t default_value = (1 << 16) - 1;
-    uint16_t data_size = I2CReaderWithAddr::read_data_size();
-    if (data_size < 4) {
-        return pos_on_fail;
-    }
-    delayMicroseconds(10);
-    uint8_t buff[data_size];
-    if (!I2CReaderWithAddr::read_data(buff, data_size)) {
-        pass_data();
-        return pos_on_fail;
-    }
-    uint16_t x = buff[0] | (buff[1] << 8);
-    uint16_t y = buff[2] | (buff[3] << 8);
-    return (
-        (x == default_value && y == default_value)
-        ? pos_on_fail
-        : CameraPos{x, y}
-    );
-}
-
-OpenMVReader openmv_i2c(0x12);
-const CameraPos pos_on_fail{0, 0};
+robo::OpenMVReader openmv_i2c(0x12);
+const robo::CameraPos pos_on_fail{0, 0};
 
 void setup()
 {
@@ -51,7 +14,7 @@ void loop()
     static uint8_t frame_count;
     static uint64_t last_time;
 
-    CameraPos pos = openmv_i2c.read_pos(pos_on_fail);
+    robo::CameraPos pos = openmv_i2c.read_pos(pos_on_fail);
     if (pos != pos_on_fail) {
         Serial.println(pos.to_string());
     }
@@ -62,7 +25,7 @@ void loop()
         last_time = cur_time;
         frame_count = 0;
         //Serial.print("            ");
-        Serial.println(fps);
+        //Serial.println(fps);
     }
     delay(1); // Don't loop too quickly.
 }
