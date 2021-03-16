90e96339202a3cf2fd050f1ef271badecaa42c42
H1rono <hronok66@gmail.com>
2021-03-16 07:47:00 +0000

    modified main2

--- Arduino/main2/main2.ino
+++ Arduino/main2/main2.ino
@@ -5,6 +5,38 @@ namespace info {
     using namespace robo::move_info;
 }
 
+class InfoPtr {
+private:
+    using robo::move_info::MoveInfo*;
+    MoveInfo *_info;
+    inline void _del() {
+        if (_info != NULL) delete _info;
+    }
+
+public:
+    InfoPtr() : _info(new robo::move_info::Stop()) {}
+    InfoPtr(MoveInfo *info) : _info(info) {}
+    InfoPtr(const InfoPtr &) = delete;
+    ~InfoPtr() { _del(); }
+    InfoPtr& operator = (const InfoPtr &ptr) = delete;
+    InfoPtr& operator = (MoveInfo *ptr) {
+        _del();
+        _info = ptr;
+        return *this;
+    }
+
+    operator bool () const { return _info != NULL; }
+
+    bool operator == (const InfoPtr &ptr) const { return ptr.ptr() == _info; }
+    bool operator != (const InfoPtr &ptr) const { return ptr.ptr() != _info; }
+
+    bool operator == (MoveInfo *pinfo) const { return pinfo == _info; }
+    bool operator != (MoveInfo *pinfo) const { return pinfo != _info; }
+
+    MoveInfo * operator -> () const { return _info; }
+    MoveInfo * ptr() const { return _info; }
+};
+
 namespace omv {
     using namespace robo::openmv;
 }
@@ -13,7 +45,7 @@ constexpr double HPI = PI / 2;
 constexpr double QPI = PI / 4;
 constexpr double bno_threshold = PI / 18;
 constexpr int8_t max_speed = 100;
-info::MoveInfo *m_stop;
+InfoPtr m_info;
 robo::Motor motor(Serial2);
 
 namespace lines {
@@ -42,44 +74,40 @@ void setup() {
     bno055.setup();
 
     Serial.begin(19200);
-    motor.setup(19200);
-    m_stop = new info::Stop();
+    Serial2.begin(19200);
+    m_info = new info::Stop();
     lcd.setup();
 }
 
 void loop() {
     using omv::Position;
-    info::MoveInfo *m_info = m_stop;
     #define BIND(_name_) w_ ## _name_ = robo::LineSensor::iswhite(lines::_name_.read())
     bool BIND(left), BIND(right), BIND(back);
     #undef BIND
     omv::Frame *frame = mv_reader.read_frame();
     Position *ball_pos = frame == NULL ? NULL : frame->ball_pos;
 
-    if (w_left || w_right || w_back) {
-        // 線を踏んだ
+    if (w_left || w_right || w_back) { // 線を踏んだ
         double d = 0.0;
         #define BIND(_name_) e_ ## _name_ = echos::_name_.read()
         uint16_t BIND(left), BIND(right), BIND(back);
         #undef BIND
-        bool lt_lr = e_left < e_right,
-            lt_br  = e_back < e_right,
-            lt_bl  = e_back < e_left;
-        d = (lt_br && lt_bl)
+        d = (e_back < e_right && e_back < e_left)
             ? 0.0 // 後ろの壁が一番近い => 前に進む
-            : lt_lr ? -HPI : HPI; // 左の方が近い ? 右に進む : 左に進む
+            : e_left < e_right ? -HPI : HPI; // 左の方が近い ? 右に進む : 左に進む
         m_info = new info::Translate(d, max_speed);
         goto MOTOR;
     }
+
     // bno
     {
         double bno_dir = bno055.get_geomag_direction();
         double adir = abs(bno_dir);
         if (adir > bno_threshold) {
             m_info = new info::Rotate(bno_dir > 0, int8_t(adir * 19 + 40));
-        // (adir - 0) / (PI - 0) * (100 - 40) + 40
-        // -> adir * 19 + 40
-        goto MOTOR;
+            // (adir - 0) / (PI - 0) * (100 - 40) + 40
+            // -> adir * 19 + 40
+            goto MOTOR;
         }
     }
 
@@ -88,15 +116,16 @@ void loop() {
         double ball_dir = omv::pos2dir(*ball_pos);
         m_info = new info::Translate(robo::V2_double::from_polar_coord(ball_dir * 3 / 2, max_speed));
     } else {
+        m_info = new Stop();
     }
 
     MOTOR: {
-        m_info->apply(motor);
+        if (m_info) m_info->apply(motor);
     }
 
     LOG: {
         char buff[64] = "";
-        m_info->to_string(buff);
+        if (m_info) m_info->to_string(buff);
         Serial.println(buff);
         for (uint8_t pin = 1; pin <= 4; pin++) Serial.println(motor.get_power_str(pin));
         lcd.clear();
@@ -104,5 +133,5 @@ void loop() {
     }
 
     END:
-    if (m_info != NULL && m_info != m_stop) delete m_info;
+    // if (m_info != NULL && m_info != m_stop) delete m_info;
 }
